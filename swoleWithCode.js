30977c2f480d88430a28ec132b58247e8caf9ae3
C-Das <chinmay98@gmail.com>
2016-06-01 23:15:58 -0400

    fixed rendering into the table

--- swoleWithCode.js
+++ swoleWithCode.js
@@ -1,15 +1,15 @@
 angular.module("swole",[]).
   controller ("swoleController", function($scope){
-    var details = [];
+    $scope.details = [];
     $scope.add = function () {
       console.log("Add hit");
-      details.push ({
+      $scope.details.push ({
         date : $scope.date,
         muscleGroup : "Test",
         exerciseName : $scope.exerciseName,
         weight : $scope.weight,
         reps : $scope.reps
       })
-      console.log(details);
+      console.log($scope.details);
     }
   });
